ccbbf76e861c41acc8bf0cc9218d28aa41e6f113
Tommy van der Vorst <tommy@pixelspark.nl>
2009-11-19 21:11:34 +0000

    - Licentie-header toegevoegd; Pyramid is open-source onder een BSD-achtige licentie
    git-svn-id: https://svn.pixelspark.nl/public/pyramid/trunk@3 ccbbcea5-15bd-4a1c-9773-a053b20246a8

--- include/header.inc.php
+++ include/header.inc.php
@@ -1,13 +1,50 @@
+<?php
+/*************************************************************************************
+	Pyramid - an online competition system for large groups 
+    Copyright (C) 2009, Tommy van der Vorst (tommy at pixelspark dot nl)
+    All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without modification,
+	are permitted provided that the following conditions are met:
+	
+	* Redistributions of source code must retain the above copyright notice, this 
+	  list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright notice, this
+	  list of conditions and the following disclaimer in the documentation and/or 
+	  other materials provided with the distribution.
+	* Neither the name of the original developer(s) nor the names of its contributors
+	   may be used to endorse or promote products derived from this software without 
+	   specific prior written permission.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+	ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*************************************************************************************/
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- Pyramid - an online competition system for large groups. Copyright (C) 2009, Tommy van der Vorst (pixelspark.nl) -->
 <html>
   <head>
-    <title>Intermate: <?php echo $title ?></title>
+    <title><?php echo $Config['title'] ?>: <?php echo $title ?></title>
     <link rel="stylesheet" href="<?php echo $Base ?>/default.css" type="text/css" />
     <script type="text/javascript" src="<?php echo $Base ?>/jquery.js"></script>
   </head>
   
   <body>
     <!--[if lt IE 7]>
-    <div style="background-color:red;font-weight:bold;color:white;">Hey! Je gebruikt een oude, stuk onveiligere versie van Internet Explorer. Download de nieuwe bij <a href="http://www.microsoft.com">Microsoft</a> of download meteen 
-    <a href="http://www.getfirefox.com">Firefox</a>. Mogelijk werkt de planner nu niet zo goed...</div>
-    <![endif]-->          
+    <div style="background-color:red;font-weight:bold;color:white;">Hey! Je gebruikt een oude, stuk onveiligere
+    versie van Internet Explorer. Download de nieuwe bij <a href="http://		www.microsoft.com">Microsoft</a> of download 
+    meteen <a href="http://www.getfirefox.com">Firefox</a>. Mogelijk werkt de planner nu niet zo goed...</div>
+    <![endif]-->         
+    <div class="links">
+    	<?php foreach($Config['links'] as $k=>$v) { ?>
+			<a href="<?php echo $v ?>"><?php echo $k ?></a>
+		<?php } ?> 
+	</div> 
--- include/page.inc.php
+++ include/page.inc.php
@@ -1,9 +1,36 @@
 <?php
-$DB = mysql_connect("localhost", "web", "w3bw3b");
-mysql_select_db("pyramid",$DB);
+/*************************************************************************************
+	Pyramid - an online competition system for large groups 
+    Copyright (C) 2009, Tommy van der Vorst (tommy at pixelspark dot nl)
+    All rights reserved.
 
-$Base = "/pyramid";
-$TablePrefix = "pyramid_";
+	Redistribution and use in source and binary forms, with or without modification,
+	are permitted provided that the following conditions are met:
+	
+	* Redistributions of source code must retain the above copyright notice, this 
+	  list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright notice, this
+	  list of conditions and the following disclaimer in the documentation and/or 
+	  other materials provided with the distribution.
+	* Neither the name of the original developer(s) nor the names of its contributors
+	   may be used to endorse or promote products derived from this software without 
+	   specific prior written permission.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+	ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*************************************************************************************/
+require_once("config.inc.php");
+
+$DB = mysql_connect($Config["db.host"], $Config["db.user"], $Config["db.password"]);
+mysql_select_db($Config["db.name"], $DB);
 
 function RequireValidPassword($username, $password) {
 	if(!($username==$password)) {
@@ -27,9 +54,9 @@ function Get($paramName) {
 
 function Query($q, $v=array()) {
 	global $DB;
-	global $TablePrefix;
+	global $Config;
 	
-	$q = str_replace("%%",$TablePrefix,$q);
+	$q = str_replace("%%",$Config["db.prefix"],$q);
 	
 	if(!is_array($v)) {
 		if(func_num_args()>1) {
@@ -57,7 +84,6 @@ function Query($q, $v=array()) {
 	}
 	
 	$result = mysql_query($q, $DB);
-	//echo "<code>".htmlentities($q)."</code>: ".mysql_affected_rows()." affected<hr/>";
 	
 	if(mysql_errno($DB)) {
            Error(mysql_error($DB)."<br/><br/>Query: <pre>$q</pre>");
@@ -76,7 +102,8 @@ function Query($q, $v=array()) {
 }
 
 function Head($title,$links=array()) {
-	global $Base;
+	global $Config;
+	$Base = $Config["base"];
 	$title = htmlentities($title);
 	require_once("header.inc.php");
 }
